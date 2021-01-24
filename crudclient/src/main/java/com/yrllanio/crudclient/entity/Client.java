b311da6ecbd47788474972e4cc06b249a346a723
yrllanio <yrlanio@gmail.com>
2021-01-24 20:35:10 -0300

    Client created

---
+++ crudclient/src/main/java/com/yrllanio/crudclient/entity/Client.java
@@ -0,0 +1,102 @@
+package com.yrllanio.crudclient.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer clildren;
+	
+	public Client() {
+	}
+
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer clildren) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.clildren = clildren;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getClildren() {
+		return clildren;
+	}
+
+	public void setClildren(Integer clildren) {
+		this.clildren = clildren;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
