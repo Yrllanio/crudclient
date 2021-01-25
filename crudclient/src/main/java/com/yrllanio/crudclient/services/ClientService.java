aa074d816a7ea3ae41e78b7f0e88c6e6e7c7a0c8
yrllanio <yrlanio@gmail.com>
2021-01-25 18:09:56 -0300

    ClientDTO

--- crudclient/src/main/java/com/yrllanio/crudclient/services/ClientService.java
+++ crudclient/src/main/java/com/yrllanio/crudclient/services/ClientService.java
@@ -1,21 +1,28 @@
 package com.yrllanio.crudclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.yrllanio.crudclient.dto.ClientDTO;
 import com.yrllanio.crudclient.entity.Client;
-import com.yrllanio.crudclient.repositories.ClientRepository;	
+import com.yrllanio.crudclient.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
-	public List<Client> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+
 	}
 
 }
